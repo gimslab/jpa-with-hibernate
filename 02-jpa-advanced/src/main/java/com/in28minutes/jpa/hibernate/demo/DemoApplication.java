261269e063bf040f4edcfbac4f066903a7038e7f
gimslab <gimslab.com@gmail.com>
2019-03-27 22:43:59 +0900

    uncomment

--- 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/DemoApplication.java
+++ 02-jpa-advanced/src/main/java/com/in28minutes/jpa/hibernate/demo/DemoApplication.java
@@ -1,7 +1,10 @@
 package com.in28minutes.jpa.hibernate.demo;
 
 import java.math.BigDecimal;
+import java.util.ArrayList;
+import java.util.List;
 
+import com.in28minutes.jpa.hibernate.demo.entity.*;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,8 +12,6 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import com.in28minutes.jpa.hibernate.demo.entity.FullTimeEmployee;
-import com.in28minutes.jpa.hibernate.demo.entity.PartTimeEmployee;
 import com.in28minutes.jpa.hibernate.demo.repository.CourseRepository;
 import com.in28minutes.jpa.hibernate.demo.repository.EmployeeRepository;
 import com.in28minutes.jpa.hibernate.demo.repository.StudentRepository;
@@ -35,22 +36,21 @@ public class DemoApplication implements CommandLineRunner {
 
 	@Override
 	public void run(String... arg0) throws Exception {
-		// studentRepository.saveStudentWithPassport();
-		// repository.playWithEntityManager();
-		// courseRepository.addHardcodedReviewsForCourse();
-		// List<Review> reviews = new ArrayList<>();
+		 studentRepository.saveStudentWithPassport();
+		 courseRepository.playWithEntityManager();
+		 courseRepository.addHardcodedReviewsForCourse();
+		 List<Review> reviews = new ArrayList<>();
 
-		// reviews.add(new Review("5", "Great Hands-on Stuff."));
-		// reviews.add(new Review("5", "Hatsoff."));
+		 reviews.add(new Review(ReviewRating.FIVE, "Great Hands-on Stuff."));
+		 reviews.add(new Review(ReviewRating.FIVE, "Hatsoff."));
 
-		// courseRepository.addReviewsForCourse(10003L, reviews );
-		// studentRepository.insertHardcodedStudentAndCourse();
-		// studentRepository.insertStudentAndCourse(new Student("Jack"),
-		// new Course("Microservices in 100 Steps"));
+		 courseRepository.addReviewsForCourse(10003L, reviews );
+		 studentRepository.insertHardcodedStudentAndCourse();
+		 studentRepository.insertStudentAndCourse(new Student("Jack"),
+		 new Course("Microservices in 100 Steps"));
 
 		// Jack FullTimeEmployee salary - 10000$
 		// Jill PartTimeEmployee - 50$ per hour
-		/*
 		employeeRepository.insert(new PartTimeEmployee("Jill", new BigDecimal("50")));
 		employeeRepository.insert(new FullTimeEmployee("Jack", new BigDecimal("10000")));
 
@@ -58,6 +58,6 @@ public class DemoApplication implements CommandLineRunner {
 				employeeRepository.retrieveAllFullTimeEmployees());
 		
 		logger.info("Part Time Employees -> {}", 
-				employeeRepository.retrieveAllPartTimeEmployees());*/
+				employeeRepository.retrieveAllPartTimeEmployees());
 	}
 }
